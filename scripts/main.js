c768ef9d63c19a69495da20a62a598ddee779c36
Pascal Hanke <pascal.hanke@yahoo.de>
2019-11-01 10:28:04 +0100

    final

--- scripts/main.js
+++ scripts/main.js
@@ -118,3 +118,10 @@ function draw() {
         restartTimeWindow();
     }
 }
+
+window.addEventListener("keydown", function (e) {
+    // space and arrow keys
+    if ([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
