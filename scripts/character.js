c74e4de3826d1c42ec8178f752fdb9a363881a74
Pascal Hanke <pascal.hanke@yahoo.de>
2019-11-06 22:19:17 +0100

    fixes

--- scripts/character.js
+++ scripts/character.js
@@ -29,257 +29,355 @@ let SpriteSheetGirlThrowingBackward;
 let girlAnimationThrowingBackward;
 
 class Girl {
-    constructor(xSize, ySize) {
-        this.width = xSize;
-        this.height = ySize;
-        this.velocity = 0;
-        this.gravity = 0.4;
-        this.jumpCount = 0;
-        this.snowballs = [];
+  constructor(xSize, ySize) {
+    this.width = xSize;
+    this.height = ySize;
+    this.velocity = 0;
+    this.gravity = 0.4;
+    this.jumpCount = 0;
+    this.snowballs = [];
+  }
+
+  setup() {
+    this.y = girlY;
+
+    this.originalY = this.y;
+
+    SpriteSheetGirlStandingForward = loadSpriteSheet(
+      "Assets/Spritesheets/Stand_Resize_FW.png",
+      115,
+      166,
+      50
+    );
+
+    girlAnimationStandingForward = loadAnimation(
+      SpriteSheetGirlStandingForward
+    );
+
+    SpriteSheetGirlStandingBackward = loadSpriteSheet(
+      "Assets/Spritesheets/Stand_Resize_BW.png",
+      115,
+      166,
+      50
+    );
+    girlAnimationStandingBackward = loadAnimation(
+      SpriteSheetGirlStandingBackward
+    );
+
+    SpriteSheetGirlJumpingForward = loadSpriteSheet(
+      "Assets/Spritesheets/Jump_Resize_FW.png",
+      115,
+      166,
+      25
+    );
+    girlAnimationJumpingForward = loadAnimation(SpriteSheetGirlJumpingForward);
+
+    SpriteSheetGirlJumpingBackward = loadSpriteSheet(
+      "Assets/Spritesheets/Jump_Resize_BW.png",
+      115,
+      166,
+      25
+    );
+    girlAnimationJumpingBackward = loadAnimation(
+      SpriteSheetGirlJumpingBackward
+    );
+
+    SpriteSheetGirlWalkingForward = loadSpriteSheet(
+      "Assets/Spritesheets/Walk_2_Resize_FW.png",
+      115,
+      166,
+      50
+    );
+    girlAnimationWalkingForward = loadAnimation(SpriteSheetGirlWalkingForward);
+
+    SpriteSheetGirlWalkingBackward = loadSpriteSheet(
+      "Assets/Spritesheets/Walk_2_Resize_reverse.png",
+      115,
+      166,
+      50
+    );
+    girlAnimationWalkingBackward = loadAnimation(
+      SpriteSheetGirlWalkingBackward
+    );
+
+    SpriteSheetGirlPushingForward = loadSpriteSheet(
+      "Assets/Spritesheets/Push_Resize_FW.png",
+      129,
+      166,
+      50
+    );
+    girlAnimationPushingForward = loadAnimation(SpriteSheetGirlPushingForward);
+
+    SpriteSheetGirlPushingBackward = loadSpriteSheet(
+      "Assets/Spritesheets/Push_Resize_BW.png",
+      129,
+      166,
+      50
+    );
+    girlAnimationPushingBackward = loadAnimation(
+      SpriteSheetGirlPushingBackward
+    );
+
+    SpriteSheetGirlBreakingIceForward = loadSpriteSheet(
+      "Assets/Spritesheets/Ice_Resize_FW.png",
+      115,
+      166,
+      50
+    );
+    girlAnimationBreakingIceForward = loadAnimation(
+      SpriteSheetGirlBreakingIceForward
+    );
+
+    SpriteSheetGirlBreakingIceBackward = loadSpriteSheet(
+      "Assets/Spritesheets/Ice_Resize _BW.png",
+      115,
+      166,
+      50
+    );
+    girlAnimationBreakingIceBackward = loadAnimation(
+      SpriteSheetGirlBreakingIceBackward
+    );
+
+    SpriteSheetGirlThrowingForward = loadSpriteSheet(
+      "Assets/Spritesheets/Throw_Resize_FW.png",
+      115,
+      166,
+      25
+    );
+    girlAnimationThrowingForward = loadAnimation(
+      SpriteSheetGirlThrowingForward
+    );
+
+    SpriteSheetGirlThrowingBackward = loadSpriteSheet(
+      "Assets/Spritesheets/Throw_Resize_BW.png",
+      115,
+      166,
+      25
+    );
+    girlAnimationThrowingBackward = loadAnimation(
+      SpriteSheetGirlThrowingBackward
+    );
+  }
+
+  draw() {
+    //JUMPING--------------------------->>>
+    this.velocity += this.gravity;
+    this.y += this.velocity;
+    /*   girlY = this.originalY; */
+    if (this.y > this.originalY) {
+      this.y = this.originalY;
+
+      this.jumpCount = 0;
+      jumpState = "no";
     }
 
-    setup() {
-        this.y = girlY;
-
-        this.originalY = this.y;
-
-
-        SpriteSheetGirlStandingForward = loadSpriteSheet("Assets/Spritesheets/Stand_Resize_FW.png", 115, 166, 50);
-
-        girlAnimationStandingForward = loadAnimation(SpriteSheetGirlStandingForward);
-
-        SpriteSheetGirlStandingBackward = loadSpriteSheet("Assets/Spritesheets/Stand_Resize_BW.png", 115, 166, 50);
-        girlAnimationStandingBackward = loadAnimation(SpriteSheetGirlStandingBackward);
-
-        SpriteSheetGirlJumpingForward = loadSpriteSheet("Assets/Spritesheets/Jump_Resize_FW.png", 115, 166, 25);
-        girlAnimationJumpingForward = loadAnimation(SpriteSheetGirlJumpingForward);
-
-        SpriteSheetGirlJumpingBackward = loadSpriteSheet("Assets/Spritesheets/Jump_Resize_BW.png", 115, 166, 25);
-        girlAnimationJumpingBackward = loadAnimation(SpriteSheetGirlJumpingBackward);
-
-        SpriteSheetGirlWalkingForward = loadSpriteSheet("Assets/Spritesheets/Walk_2_Resize_FW.png", 115, 166, 50);
-        girlAnimationWalkingForward = loadAnimation(SpriteSheetGirlWalkingForward);
-
-        SpriteSheetGirlWalkingBackward = loadSpriteSheet("Assets/Spritesheets/Walk_2_Resize_reverse.png", 115, 166, 50);
-        girlAnimationWalkingBackward = loadAnimation(SpriteSheetGirlWalkingBackward);
-
-        SpriteSheetGirlPushingForward = loadSpriteSheet("Assets/Spritesheets/Push_Resize_FW.png", 129, 166, 50);
-        girlAnimationPushingForward = loadAnimation(SpriteSheetGirlPushingForward);
-
-        SpriteSheetGirlPushingBackward = loadSpriteSheet("Assets/Spritesheets/Push_Resize_BW.png", 129, 166, 50);
-        girlAnimationPushingBackward = loadAnimation(SpriteSheetGirlPushingBackward);
-
-        SpriteSheetGirlBreakingIceForward = loadSpriteSheet("Assets/Spritesheets/Ice_Resize_FW.png", 115, 166, 50);
-        girlAnimationBreakingIceForward = loadAnimation(SpriteSheetGirlBreakingIceForward);
-
-        SpriteSheetGirlBreakingIceBackward = loadSpriteSheet("Assets/Spritesheets/Ice_Resize _BW.png", 115, 166, 50);
-        girlAnimationBreakingIceBackward = loadAnimation(SpriteSheetGirlBreakingIceBackward);
-
-        SpriteSheetGirlThrowingForward = loadSpriteSheet("Assets/Spritesheets/Throw_Resize_FW.png", 115, 166, 25);
-        girlAnimationThrowingForward = loadAnimation(SpriteSheetGirlThrowingForward);
-
-        SpriteSheetGirlThrowingBackward = loadSpriteSheet("Assets/Spritesheets/Throw_Resize_BW.png", 115, 166, 25);
-        girlAnimationThrowingBackward = loadAnimation(SpriteSheetGirlThrowingBackward);
-
-    }
-
-    draw() {
-
-        //JUMPING--------------------------->>>
-        this.velocity += this.gravity;
-        this.y += this.velocity;
-        /*   girlY = this.originalY; */
-        if (this.y > this.originalY) {
-            this.y = this.originalY;
-
-            this.jumpCount = 0;
-            jumpState = "no";
-        }
-
-        /*         positionColliderX = girlCollider.position.x;
+    /*         positionColliderX = girlCollider.position.x;
                 girlCollider.position.y = this.y;
                 girlCollider.position.x = canvasWidth / 2;
                 girlCollider.debug = true; */
 
-
-        //BREAKING INTO THE ICE--------------------------->>>
-        if (x < -3670 && x > -4200 && this.y === 750) {
-            icebreak = "yes";
-            if (girlAnimationBreakingIceForward.getFrame() === 49) {
-                gameMode = 5;
-            } else if (girlAnimationBreakingIceBackward.getFrame() === 49) {
-                gameMode = 5;
-            }
-        }
-
-        //IDLE ANIMATIONS GIRL--------------------------->>>
-        if (jumpState === "no" && direction === "forward" && throwState === "true") {
-            animation(girlAnimationThrowingForward, canvasWidth / 2, this.y)
-        } else if (jumpState === "no" && direction === "backward" && throwState === "true") {
-            animation(girlAnimationThrowingBackward, canvasWidth / 2, this.y)
-        } else if (jumpState === "no" && direction === "forward" && icebreak === "yes") {
-            /*  girlAnimationBreakingIceForward.rewind(); */
-            animation(girlAnimationBreakingIceForward, canvasWidth / 2, this.y)
-            girlAnimationBreakingIceForward.looping = false;
-        } else if (jumpState === "no" && direction === "backward" && icebreak === "yes") {
-            /*       girlAnimationBreakingIceBackward.rewind(); */
-            animation(girlAnimationBreakingIceBackward, canvasWidth / 2, this.y);
-            girlAnimationBreakingIceBackward.looping = false;
-        } else if (jumpState === "no" && direction === "forward" && movingState === "moving" && pushState === "push") {
-            animation(girlAnimationPushingForward, canvasWidth / 2, this.y);
-        } else if (jumpState === "no" && direction === "backward" && movingState === "moving" && pushState === "push") {
-            animation(girlAnimationPushingBackward, canvasWidth / 2, this.y)
-        } else if (jumpState === "no" && direction === "forward" && movingState === "moving") {
-            animation(girlAnimationWalkingForward, canvasWidth / 2, this.y)
-        } else if (jumpState === "no" && direction === "backward" && movingState === "moving") {
-            animation(girlAnimationWalkingBackward, canvasWidth / 2, this.y)
-        } else if (jumpState === "yes" && direction === "forward") {
-            animation(girlAnimationJumpingForward, canvasWidth / 2, this.y);
-        } else if (jumpState === "yes" && direction === "backward") {
-            animation(girlAnimationJumpingBackward, canvasWidth / 2, this.y);
-        } else if (direction === "forward") {
-            push();
-            if (gameMode === 3) {
-                tint(255, 20, 0, 150)
-            };
-            animation(girlAnimationStandingForward, canvasWidth / 2, this.y);
-            pop();
-        } else if (direction === "backward") {
-            push();
-            if (gameMode === 3) {
-                tint(255, 20, 0, 150)
-            };
-            animation(girlAnimationStandingBackward, canvasWidth / 2, this.y);
-            pop();
-        }
+    //BREAKING INTO THE ICE--------------------------->>>
+    if (x < -3670 && x > -4200 && this.y === 750) {
+      icebreak = "yes";
+      if (girlAnimationBreakingIceForward.getFrame() === 49) {
+        gameMode = 5;
+      } else if (girlAnimationBreakingIceBackward.getFrame() === 49) {
+        gameMode = 5;
+      }
     }
 
-    jump() {
-        if (this.jumpCount < 1) {
-            this.velocity = -12;
-            this.jumpCount++;
-            jumpState = "yes";
-        }
+    //IDLE ANIMATIONS GIRL--------------------------->>>
+    if (
+      jumpState === "no" &&
+      direction === "forward" &&
+      throwState === "true"
+    ) {
+      animation(girlAnimationThrowingForward, canvasWidth / 2, this.y);
+    } else if (
+      jumpState === "no" &&
+      direction === "backward" &&
+      throwState === "true"
+    ) {
+      animation(girlAnimationThrowingBackward, canvasWidth / 2, this.y);
+    } else if (
+      jumpState === "no" &&
+      direction === "forward" &&
+      icebreak === "yes"
+    ) {
+      /*  girlAnimationBreakingIceForward.rewind(); */
+      animation(girlAnimationBreakingIceForward, canvasWidth / 2, this.y);
+      girlAnimationBreakingIceForward.looping = false;
+    } else if (
+      jumpState === "no" &&
+      direction === "backward" &&
+      icebreak === "yes"
+    ) {
+      /*       girlAnimationBreakingIceBackward.rewind(); */
+      animation(girlAnimationBreakingIceBackward, canvasWidth / 2, this.y);
+      girlAnimationBreakingIceBackward.looping = false;
+    } else if (
+      jumpState === "no" &&
+      direction === "forward" &&
+      movingState === "moving" &&
+      pushState === "push"
+    ) {
+      animation(girlAnimationPushingForward, canvasWidth / 2, this.y);
+    } else if (
+      jumpState === "no" &&
+      direction === "backward" &&
+      movingState === "moving" &&
+      pushState === "push"
+    ) {
+      animation(girlAnimationPushingBackward, canvasWidth / 2, this.y);
+    } else if (
+      jumpState === "no" &&
+      direction === "forward" &&
+      movingState === "moving"
+    ) {
+      animation(girlAnimationWalkingForward, canvasWidth / 2, this.y);
+    } else if (
+      jumpState === "no" &&
+      direction === "backward" &&
+      movingState === "moving"
+    ) {
+      animation(girlAnimationWalkingBackward, canvasWidth / 2, this.y);
+    } else if (jumpState === "yes" && direction === "forward") {
+      animation(girlAnimationJumpingForward, canvasWidth / 2, this.y);
+    } else if (jumpState === "yes" && direction === "backward") {
+      animation(girlAnimationJumpingBackward, canvasWidth / 2, this.y);
+    } else if (direction === "forward") {
+      push();
+      if (gameMode === 3) {
+        tint(255, 20, 0, 150);
+      }
+      animation(girlAnimationStandingForward, canvasWidth / 2, this.y);
+      pop();
+    } else if (direction === "backward") {
+      push();
+      if (gameMode === 3) {
+        tint(255, 20, 0, 150);
+      }
+      animation(girlAnimationStandingBackward, canvasWidth / 2, this.y);
+      pop();
     }
+  }
 
-    throwingBall() {
-
-        if (snowballAppearance === "true" && this.snowballs.length) {
-
-            for (let i = 0; i < this.snowballs.length; i++) {
-                this.snowballs[i].draw();
-                if (keyIsDown(83)) {
-                    snowVelocity -= 1
-                }
-            }
-
-
-            snowVelocity += snowGravity;
-            snowball_Y += snowVelocity;
-
-            if (snowball_Y > 600 && snowball_Y < 690 && (x - snowball_X) < -7000 && (x - snowball_X) > -7090) {
-                trenchOpen = "true";
-                hitTarget = "true";
-                targetAnimation.rewind();
-                snowGravity = 30;
-                hitArr.push(1);
-            }
-
-            if (snowball_Y > canvasHeight - 100) {
-                snowballAppearance = "false"; // state of showing the ball
-                this.snowballs = []
-                snowball_X = canvasWidth / 2 + 10;
-            }
-            snowball_X += snowBallDirection
-
-        }
-        if (keyIsDown(83) && fallDown < 1) {
-            snowVelocity = -10;
-            fallDown = 1;
-        }
-
-
-
-
+  jump() {
+    if (this.jumpCount < 1) {
+      this.velocity = -12;
+      this.jumpCount++;
+      jumpState = "yes";
     }
+  }
 
+  throwingBall() {
+    if (snowballAppearance === "true" && this.snowballs.length) {
+      for (let i = 0; i < this.snowballs.length; i++) {
+        this.snowballs[i].draw();
+        if (keyIsDown(83)) {
+          snowVelocity -= 1;
+        }
+      }
+
+      snowVelocity += snowGravity;
+      snowball_Y += snowVelocity;
+
+      if (
+        snowball_Y > 600 &&
+        snowball_Y < 690 &&
+        x - snowball_X < -7000 &&
+        x - snowball_X > -7090
+      ) {
+        trenchOpen = "true";
+        hitTarget = "true";
+        targetAnimation.rewind();
+        snowGravity = 30;
+        hitArr.push(1);
+      }
+
+      if (snowball_Y > canvasHeight - 100) {
+        snowballAppearance = "false"; // state of showing the ball
+        this.snowballs = [];
+        snowball_X = canvasWidth / 2 + 10;
+      }
+      snowball_X += snowBallDirection;
+    }
+    if (keyIsDown(83) && fallDown < 1) {
+      snowVelocity = -10;
+      fallDown = 1;
+    }
+  }
 }
 
-
 function keyPressed() {
-    //----JUMPING
-    if (keyCode === 32) {
-        girl.jump()
-    }
+  //----JUMPING
+  if (keyCode === 32 && gameMode === 1) {
+    girl.jump();
+    girlAnimationJumpingForward.rewind();
+    girlAnimationJumpingBackward.rewind();
+  }
 
-    //----PUSHING
-    else if (keyCode === 87) {
-        pushState = "push";
-        movingSpeed = 0.5;
-    }
+  //----PUSHING
+  else if (keyCode === 87) {
+    pushState = "push";
+    movingSpeed = 0.5;
+  }
 
-    //------PUSHRELEASE
-    if (keyCode !== 87) {
-        pushState = "no";
-        movingSpeed = 1.5;
-    }
+  //------PUSHRELEASE
+  if (keyCode !== 87) {
+    pushState = "no";
+    movingSpeed = 1.5;
+  }
 
-    //-----SHOOTING EVENT
+  //-----SHOOTING EVENT
 
+  if (shootingCount === 0) {
+    //----SETTING SHOOTING DIRECTIION
+    if (direction === "forward") {
+      snowBallDirection = 30;
+    } else snowBallDirection = -30;
 
+    //------SHOOTING RELEASE
+    if (keyCode === 83) {
+      snowVelocity = 0;
+      setTimeout(() => {
+        girl.snowballs.push(snowball);
+        snowballAppearance = "true";
+        setTimeout(() => {
+          throwState = "false";
+          girlAnimationThrowingForward.rewind();
+          girlAnimationThrowingBackward.rewind();
+        }, 300);
+      }, 500);
 
+      throwState = "true";
 
+      snowball_Y = girl.originalY - 50;
 
+      snowball_X = canvasWidth / 2 + 10;
+      snowVelocity = 0;
+      fallDown = 0;
 
+      let i = 0;
+      shootingCount = 1;
 
-    if (shootingCount === 0) {
-        //----SETTING SHOOTING DIRECTIION
-        if (direction === "forward") {
-            snowBallDirection = 30;
-        } else snowBallDirection = -30;
-
-        //------SHOOTING RELEASE
-        if (keyCode === 83) {
-
-            snowVelocity = 0
-            setTimeout(() => {
-                girl.snowballs.push(snowball);
-                snowballAppearance = "true";
-                setTimeout(() => {
-                    throwState = "false"
-                    girlAnimationThrowingForward.rewind()
-                    girlAnimationThrowingBackward.rewind()
-                }, 300)
-            }, 500)
-
-            throwState = "true";
-
-            snowball_Y = girl.originalY - 50;
-
-            snowball_X = canvasWidth / 2 + 10;
-            snowVelocity = 0;
-            fallDown = 0;
-
-            let i = 0;
-            shootingCount = 1;
-
-            //----SHOOTING RESET INTERVAL
-            setTimeout(() => {
-                i++
-                if (i >= 1) {
-                    snowVelocity = 0;
-                    fallDown = 0;
-                    shootingCount = 0;
-                    snowGravity = 1.2;
-                }
-            }, 1150);
+      //----SHOOTING RESET INTERVAL
+      setTimeout(() => {
+        i++;
+        if (i >= 1) {
+          snowVelocity = 0;
+          fallDown = 0;
+          shootingCount = 0;
+          snowGravity = 1.2;
         }
+      }, 1150);
     }
+  }
 }
 
 function keyReleased() {
-    if (keyCode !== 87) {
-        pushState = "no";
-        movingSpeed = 1.5;
-    }
+  if (keyCode !== 87) {
+    pushState = "no";
+    movingSpeed = 1.5;
+  }
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,127 +1,126 @@
-let backgroundLast = new BackgroundLast;
-let backgroundsecondLast = new BackgroundSecondLast;
-let middleGround = new MiddleGround;
-let fog = new Fog;
-let forGround = new ForGround;
-let lake = new LakeSolo;
-let fenceFront = new FenceFront;
-let fenceDoor = new FenceDoor;
-let house = new House;
-let houseDoor = new HouseDoor;
-let target = new Target;
-let timer = new Timer;
+let backgroundLast = new BackgroundLast();
+let backgroundsecondLast = new BackgroundSecondLast();
+let middleGround = new MiddleGround();
+let fog = new Fog();
+let forGround = new ForGround();
+let lake = new LakeSolo();
+let fenceFront = new FenceFront();
+let fenceDoor = new FenceDoor();
+let house = new House();
+let houseDoor = new HouseDoor();
+let target = new Target();
+let timer = new Timer();
 
-let girl = new Girl;
-let snow = new Snow;
-let obstacle1 = new Obstacle1;
-let tree = new ObstacleTree;
-let snowball = new Snowball;
+let girl = new Girl();
+let snow = new Snow();
+let obstacle1 = new Obstacle1();
+let tree = new ObstacleTree();
+let snowball = new Snowball();
 /* let fenceDoor = new FenceDoor; */
 
 let wind;
 
 function preload() {
-    //BG_Images
-    BGImageLast = loadImage("Assets/Background_Last_Layer_2.png");
-    BGImageSecondLast = loadImage("Assets/Background_Second_Last_Layer.png");
-    ImgMiddleGround = loadImage("Assets/MiddleGround__Layer_191031.png");
-    ImgLakeSolo = loadImage("Assets/BackgroundLake_Solo.png");
-    ImgFenceFront = loadImage("Assets/Fence_Front.png");
-    ImgFog = loadImage("Assets/fog.png");
-    ImgForeground = loadImage("Assets/Foreground_new2.png");
+  //BG_Images
+  BGImageLast = loadImage("Assets/Background_Last_Layer_2.png");
+  BGImageSecondLast = loadImage("Assets/Background_Second_Last_Layer.png");
+  ImgMiddleGround = loadImage("Assets/MiddleGround__Layer_191031.png");
+  ImgLakeSolo = loadImage("Assets/BackgroundLake_Solo.png");
+  ImgFenceFront = loadImage("Assets/Fence_Front.png");
+  ImgFog = loadImage("Assets/fog.png");
+  ImgForeground = loadImage("Assets/Foreground_new2.png");
 
-    //SOUNDS
+  //SOUNDS
 
+  //Obstacles
+  obstacle1.setup();
+  tree.setup();
+  fenceDoor.setup();
+  house.setup();
+  houseDoor.setup();
+  target.setup();
 
-
-    //Obstacles
-    obstacle1.setup()
-    tree.setup()
-    fenceDoor.setup()
-    house.setup()
-    houseDoor.setup()
-    target.setup()
-
-    //Spritesheets Girl
-    girl.setup();
-
+  //Spritesheets Girl
+  girl.setup();
 }
 
 function setup() {
-    let canvas = createCanvas(canvasWidth, canvasHeight);
-    canvas.parent("container");
+  let canvas = createCanvas(canvasWidth, canvasHeight);
+  canvas.parent("container");
 
-    //Animation Delays
-    girlAnimationStandingForward.frameDelay = 2;
-    girlAnimationStandingBackward.frameDelay = 2;
-    girlAnimationJumpingForward.frameDelay = 2;
-    girlAnimationJumpingBackward.frameDelay = 2;
-    girlAnimationWalkingForward.frameDelay = 2;
-    girlAnimationWalkingBackward.frameDelay = 2;
-    girlAnimationPushingForward.frameDelay = 2;
-    girlAnimationPushingBackward.frameDelay = 2;
-    girlAnimationBreakingIceForward.frameDelay = 2;
-    girlAnimationBreakingIceBackward.frameDelay = 2;
-    girlAnimationThrowingForward.frameDelay = 2;
-    girlAnimationThrowingBackward.frameDelay = 2;
-    fenceDoorOpenAnimation.frameDelay = 2;
-    fenceDoorOpeningAnimation.frameDelay = 2;
-    fenceDoorClosedAnimation.frameDelay = 2;
-    houseSpritesheet.frameDelay = 2;
-    houseAnimation.frameDelay = 2;
-    houseDoorClosedSpritesheet.frameDelay = 2;
-    houseDoorAnimationclosed.frameDelay = 2;
-    houseDoorOpeningSpritesheet.frameDelay = 2;
-    houseDoorOpeningAnimation.frameDelay = 2;
-    targetSpritesheet.frameDelay = 2;
-    targetAnimation.frameDelay = 2;
-    targetAnimationStill.frameDelay = 2;
+  //Animation Delays
+  girlAnimationStandingForward.frameDelay = 2;
+  girlAnimationStandingBackward.frameDelay = 2;
+  girlAnimationJumpingForward.frameDelay = 2;
+  girlAnimationJumpingBackward.frameDelay = 2;
+  girlAnimationWalkingForward.frameDelay = 2;
+  girlAnimationWalkingBackward.frameDelay = 2;
+  girlAnimationPushingForward.frameDelay = 2;
+  girlAnimationPushingBackward.frameDelay = 2;
+  girlAnimationBreakingIceForward.frameDelay = 2;
+  girlAnimationBreakingIceBackward.frameDelay = 2;
+  girlAnimationThrowingForward.frameDelay = 2;
+  girlAnimationThrowingBackward.frameDelay = 2;
+  fenceDoorOpenAnimation.frameDelay = 2;
+  fenceDoorOpeningAnimation.frameDelay = 2;
+  fenceDoorClosedAnimation.frameDelay = 2;
+  houseSpritesheet.frameDelay = 2;
+  houseAnimation.frameDelay = 2;
+  houseDoorClosedSpritesheet.frameDelay = 2;
+  houseDoorAnimationclosed.frameDelay = 2;
+  houseDoorOpeningSpritesheet.frameDelay = 2;
+  houseDoorOpeningAnimation.frameDelay = 2;
+  targetSpritesheet.frameDelay = 2;
+  targetAnimation.frameDelay = 2;
+  targetAnimationStill.frameDelay = 2;
 
-    document.querySelector("#restart").style.visibility = "hidden";
-    document.querySelector("#winning").style.visibility = "hidden";
-    document.querySelector("#restartIce").style.visibility = "hidden";
-    document.querySelector("#restartTime").style.visibility = "hidden";
+  document.querySelector("#restart").style.visibility = "hidden";
+  document.querySelector("#winning").style.visibility = "hidden";
+  document.querySelector("#restartIce").style.visibility = "hidden";
+  document.querySelector("#restartTime").style.visibility = "hidden";
 }
-
 
 function draw() {
+  if (gameMode > 0) {
+    document.querySelector("#start").style.visibility = "hidden";
+    background("black", 0, 0, 10000, 1000);
+    t = frameCount / 60;
+    timer.setup();
+    movingState = "still";
+    backgroundLast.draw();
+    backgroundsecondLast.draw();
+    middleGround.draw();
+    tree.draw();
+    obstacle1.draw();
+    drawSprites();
+    fenceDoor.draw();
+    house.draw();
+    target.draw();
+    girl.draw();
 
-    if (gameMode > 0) {
-        document.querySelector("#start").style.visibility = "hidden"
-        background("black", 0, 0, 10000, 1000);
-        t = frameCount / 60;
-        timer.setup();
-        movingState = "still";
-        backgroundLast.draw();
-        backgroundsecondLast.draw();
-        middleGround.draw();
-        tree.draw()
-        obstacle1.draw();
-        drawSprites();
-        fenceDoor.draw();
-        house.draw();
-        target.draw()
-        girl.draw();
+    houseDoor.draw();
 
-        houseDoor.draw();
+    girl.throwingBall();
+    lake.draw();
+    fenceFront.draw();
+    fog.draw();
+    snow.draw();
+    forGround.draw();
 
-        girl.throwingBall()
-        lake.draw();
-        fenceFront.draw();
-        fog.draw();
-        snow.draw();
-        forGround.draw();
-
-        restartWindow();
-        winningWindow();
-        restartIceWindow();
-        restartTimeWindow();
-    }
+    restartWindow();
+    winningWindow();
+    restartIceWindow();
+    restartTimeWindow();
+  }
 }
 
-window.addEventListener("keydown", function (e) {
+window.addEventListener(
+  "keydown",
+  function(e) {
     // space and arrow keys
     if ([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
-        e.preventDefault();
+      e.preventDefault();
     }
-}, false);
+  },
+  false
+);
